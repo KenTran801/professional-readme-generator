e0a5094ca048286ba72faff523e3c488aa8880eb
KenTran801 <utes.fan.tran@gmail.com>
2021-02-17 18:15:55 -0700

    Adding questions for the read.me sections.

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
+// const, let, vars
+const fs = require('fs');
+const inquirer = require('inquirer');
 // array of questions for user
+inquirer
+    .prompt([
+    // Title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter in the project title.',
+    },
+    // Project Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a general description of your project.',
+    },
+    // Project Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide the steps required to install your project?',
+    },
+    // Project Usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions and examples of use.',
+    },
+    // Project Licenses
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Please select the licenses for your project',
+        choices: [
+            'GNU',
+            'Mozilla',
+            'Apache',
+            'MIT',
+        ],
+    },
+    // Project Contributors
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please provide the contribtors or how to contribute to your project.',
+    },
+    // Project Tests
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide for tests for your application and example on how to run them.',
+    },
+    // Questions section
+        // Github Username
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please provide your GitHub username.',
+    },
+        // Email
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.',
+    },
+])
 const questions = [
     // Title
     // Project Description
@@ -7,8 +74,9 @@ const questions = [
     // Project Licenses
     // Project Contributors
     // Project Tests
-    // Github username
-    // Github email
+    // Question section
+        // Github username
+        // Email
 ];
 
 // function to write README file
